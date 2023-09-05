9d8501285774a2d6152b9dc4fbe8b5e6e88a5036
Said-Aabilla <said160aabilla@gmail.com>
2023-09-05 14:33:26 +0100

    first commit

---
+++ spring-craftsmanship/src/main/java/com/javacraftsmanship/springcraftsmanship/SpringCraftsmanshipApplication.java
@@ -0,0 +1,13 @@
+package com.javacraftsmanship.springcraftsmanship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCraftsmanshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCraftsmanshipApplication.class, args);
+	}
+
+}
---
+++ spring-craftsmanship/src/test/java/com/javacraftsmanship/springcraftsmanship/SpringCraftsmanshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacraftsmanship.springcraftsmanship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCraftsmanshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
